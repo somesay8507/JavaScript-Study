491a5b984d137cd93bc3d6f2fad60f0faa6e7378
somesay8507 <somesay8507@gmail.com>
2022-05-30 21:39:29 +0900

    day1(기본문법) 정리 완료

--- day1/main.js
+++ day1/main.js
@@ -1,9 +1,8 @@
-//간단한 프로그램 실행
-//Hello world 출력
-//반드시 문자열은 항상 ''을 넣어야 실행이 가능하다.
-console.log('Hello world');
+  console.log("Hello world");
+  //한줄 코딩이여도 꼭 세미콜론을 붙이는 습관을 기르기
+  
+  //를 붙이면 // 뒤로는 모두 주석이 됩니다.
 
+  /* 여러줄을 쓰고싶을때 */
+  
 
-//변수를 담아서 값을 출력해보기
-let name = 12;
-console.log(name);
